777f149956e240d939227f62f76b47ef405833b6
phobeetan <phobeetan@gmail.com>
2025-12-05 09:11:26 +0100

    created administrator, made basic functions for account without connection to GUI

---
+++ Users/patient.py
@@ -0,0 +1,14 @@
+class Patient(Account, prescriptionLogAccess):
+    
+    def __init__(email, password):
+        patientEmail = self.validateEmail(email)
+        patientID = self.assignID()
+        editUser()
+    def assignID():
+        patientID = rand(1,10000)
+        if patientID in patientLog.db:
+
+        
+    def verifyLogin(patientLog, email, password):
+        super.verifyLogin(log, email, password)
+
---
+++ Users/pharmacist.py
@@ -0,0 +1,7 @@
+class Pharmacist(Account, inventoryManagement, prescriptionLogAccess)
+
+    def __init__(pharmacistID, email, password)
+
+    def validateID():
+        if pharmacistID in pharmacistLog:
+            
