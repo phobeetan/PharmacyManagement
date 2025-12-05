2e351266812cbfef3d3060968f4de2775cb1dd61
Zayna <zaynadilawar@gmail.com>
2025-12-05 09:41:50 +0100

    test

--- PharmacyLogs/patientLog.py
+++ PharmacyLogs/patientLog.py
@@ -4,14 +4,14 @@ PatientLog = sqlite3.connect('PharmacyLogs/patientLog.db')
 
 cursor = PatientLog.cursor()
 
-cursor.execute("""CREATE TABLE patientLog (
-                patientID integer primary key,
-                firstName text,
-                lastName text,
-                birthday text,
-                email text,
-                password text
-                )""")
+# cursor.execute("""CREATE TABLE patientLog (
+#                 patientID integer primary key,
+#                 firstName text,
+#                 lastName text,
+#                 birthday text,
+#                 email text,
+#                 password text
+#                 )""")
 
 def add_patient(Patient):
     with PatientLog:
