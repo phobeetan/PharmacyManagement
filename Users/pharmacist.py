88bacad26f21b37afb195168b3344d330c28b10f
phobeetan <phobeetan@gmail.com>
2025-12-13 11:25:43 +0100

    pharmacist.py functions done

--- Users/pharmacist.py
+++ Users/pharmacist.py
@@ -1,7 +1,20 @@
-class Pharmacist(Account, inventoryManagement, prescriptionLogAccess)
+from verify import Verify
+from PharmacyLogs.pharmacistLog import pharmacistLog
 
-    def __init__(pharmacistID, email, password)
+log = pharmacistLog()
 
-    def validateID():
-        if pharmacistID in pharmacistLog:
-            
+
+class Pharmacist(Verify):
+    def __init__(self, email, firstName, lastName, password):
+        self.log = log
+        self.verify = Verify(log.DB_PATH)
+        self.email = email
+
+        # Validate
+        if not self.verify.validateEmail(email):
+            raise ValueError("Email Already Exists")
+        if not self.verify.validateName(firstName) or not self.verify.validateName(lastName):
+            raise ValueError("Invalid Characters in Name")
+
+        # Register
+        self.log.insert(email, firstName, lastName, password)
