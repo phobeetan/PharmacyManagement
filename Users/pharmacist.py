f779ddd7cc343e6f335db23f8fae4db7b9ac9e64
Zayna <zaynadilawar@gmail.com>
2025-12-13 13:08:21 +0100

    cleaned up imports for pharmacist

--- Users/pharmacist.py
+++ Users/pharmacist.py
@@ -1,14 +1,11 @@
 from verify import Verify
-from datetime import datetime, timedelta
-from PharmacyLogs.pharmacistLog import pharmacistLog
-from PharmacyLogs.inventoryLog import inventoryLog
-from PharmacyLogs.prescriptionLog import prescriptionLog
-from PharmacyLogs.pickUpLog import pickUpLog
+from datetime import datetime
+from PharmacyLogs import pharmacistLog, inventoryLog, prescriptionLog, pickUpLog
 
-global_pharmacist = pharmacistLog()
-global_inventory = inventoryLog()
-global_prescription = prescriptionLog()
-global_pickUp = pickUpLog()
+global_pharmacist = pharmacistLog.pharmacistLog()
+global_inventory = inventoryLog.inventoryLog()
+global_prescription = prescriptionLog.prescriptionLog()
+global_pickUp = pickUpLog.pickUpLog()
 
 class Pharmacist(Verify):
     pharmacist = global_pharmacist
@@ -17,7 +14,7 @@ class Pharmacist(Verify):
     pickUp = global_pickUp
 
     def __init__(self, pharmacistID, email, firstName, lastName, password):
-        self.verify = Verify(pharmacist.DB_PATH)
+        self.verify = Verify(self.pharmacist.DB_PATH)
         self.email = email
 
         # Validate
