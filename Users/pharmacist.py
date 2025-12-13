d3a747af31dfd7aa2489a6a1ee192291dd70eb63
Zayna <zaynadilawar@gmail.com>
2025-12-13 13:06:34 +0100

    added functionality to pharmacist

--- Users/pharmacist.py
+++ Users/pharmacist.py
@@ -1,19 +1,23 @@
 from verify import Verify
+from datetime import datetime, timedelta
 from PharmacyLogs.pharmacistLog import pharmacistLog
 from PharmacyLogs.inventoryLog import inventoryLog
 from PharmacyLogs.prescriptionLog import prescriptionLog
+from PharmacyLogs.pickUpLog import pickUpLog
 
 global_pharmacist = pharmacistLog()
 global_inventory = inventoryLog()
 global_prescription = prescriptionLog()
-
+global_pickUp = pickUpLog()
 
 class Pharmacist(Verify):
+    pharmacist = global_pharmacist
     inventory = global_inventory
     prescription = global_prescription
+    pickUp = global_pickUp
 
     def __init__(self, pharmacistID, email, firstName, lastName, password):
-        self.verify = Verify(global_pharmacist.DB_PATH)
+        self.verify = Verify(pharmacist.DB_PATH)
         self.email = email
 
         # Validate
@@ -23,13 +27,16 @@ class Pharmacist(Verify):
             raise ValueError("Invalid Characters in Name")
 
         # Register
-        self.global_pharmacist.insert(pharmacistID, email, firstName, lastName, password)
+        self.pharmacist.insert(pharmacistID, email, firstName, lastName, password)
 
     def addInventory(self, medicalName, commonName, doseAmount, bottleAmount, daysUse, doseUnits, instructions, expirationDate):
         bottleID = self.inventory.insert(medicalName, commonName, doseAmount, bottleAmount, daysUse, doseUnits, instructions, expirationDate)
         print(bottleID)
 
-    def assignMedicine(self, bottleID, patientID, needsRefill=0):
+    def assignPrescription(self, bottleID, patientID, needsRefill=0):
         prescriptionID = self.prescription.insert(bottleID, patientID, needsRefill)
         print(prescriptionID)
 
+    def updatePickUp(self, prescriptionID, pickUpDate):
+        pickUpDate = datetime.fromisoformat(pickUpDate)
+        self.pickUp.markPickedUp(prescriptionID, pickUpDate)
