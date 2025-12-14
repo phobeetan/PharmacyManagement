c71994cf9ec4cf2ca8a18bda2bb00f3eb4ab01b6
Zayna <zaynadilawar@gmail.com>
2025-12-14 15:25:45 +0100

    test

--- PharmacyLogs/inventoryLog.py
+++ PharmacyLogs/inventoryLog.py
@@ -31,7 +31,4 @@ class inventoryLog(Log):
 
         params = (medicalName, commonName, doseAmount, bottleAmount, daysUse, doseUnits, instructions, expirationDate)
 
-        with self.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute(sql, params)
-            return cursor.lastrowid
+        with self.get_connection(
